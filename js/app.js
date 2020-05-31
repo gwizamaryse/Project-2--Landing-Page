69529ae3f6324fc3faf30f4065a3bdeb86349dbf
Gwiza Maryse <gwizamaryse@gmail.com>
2020-05-31 11:02:19 +0200

    codes to click added, next is the active class

--- js/app.js
+++ js/app.js
@@ -32,6 +32,7 @@ function createNavigation() {
         navigationNode.appendChild(createNavigationItem(sectionData[i]))
     }
 }
+
 // Create navigation item
 function createNavigationItem(navigationItem) {
     let liNode = document.createElement("LI");
@@ -43,36 +44,10 @@ function createNavigationItem(navigationItem) {
     return liNode;
 }
 // Get page navigation height
-git
+function getPageHeaderHeight() {
+    return document.getElementsByClassName("page__header")[0].getBoundingClientRect().height;
+}
 // Set the active class on navigation item and section
-// function changeActiveClass() {
-//     let sections = document.getElementsByTagName("SECTION");
-//     let navigationItem = document.getElementsByTagName("LI");
-//     let isActiveSet = false;
-//     for (let i = 0; i < sections.length; i++) {
-//         let sectionBounding = sections[i].getBoundingClientRect();
-//         // Calculate the height from top
-//         let sectionHeight = sectionBounding.top + sectionBounding.height - getPageHeaderHeight();
-//         // Set the active class to the element that is first in the viewport
-//         if (isActiveSet) {
-//             sections[i].removeAttribute("class");
-//             navigationItem[i].childNodes[0].removeAttribute("class");
-//             navigationItem[i].childNodes[0].classList.add("menu__link");
-//         } else if (sectionHeight > 0) {
-//             if (sections[i].classList[0] !== "your-active-class") {
-//                 sections[i].classList.add("your-active-class");
-//             }
-//             if (navigationItem[i].childNodes[0].classList[1] !== "menu__link__active") {
-//                 navigationItem[i].childNodes[0].classList.add("menu__link__active");
-//             }
-//             isActiveSet = true;
-//         } else {
-//             sections[i].removeAttribute("class");
-//             navigationItem[i].childNodes[0].removeAttribute("class");
-//             navigationItem[i].childNodes[0].classList.add("menu__link");
-//         }
-//     }
-// }
 
 /**
  * End Helper Functions
@@ -84,11 +59,8 @@ git
 createNavigation();
 
 // Add class 'active' to section when near top of viewport
-window.onscroll = function () {
-    changeActiveClass();
-};
 
-// Scroll to anchor ID using scrollTO event
+// // Scroll to anchor ID using scrollTO event
 function scrollPage(index) {
     let sections = document.getElementsByTagName("SECTION");
     let sectionBounding = sections[index].getBoundingClientRect();
@@ -112,6 +84,3 @@ function addClickEventListenerToNavigationItems() {
 
 // Scroll to section on link click
 addClickEventListenerToNavigationItems();
-
-// Set sections as active
-changeActiveClass();
