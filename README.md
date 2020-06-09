2dd68b914b6a6271462162f085640aba2bec9979
Gwiza Maryse <gwizamaryse@gmail.com>
2020-06-09 19:03:26 +0200

    final version

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
-# Landing Page Project
+# ICE CRIME :icecream:
 
-## Table of Contents
+## Project 2 - Landing Page (Front End Developer Udacity Nanodegree)
 
-* [Instructions](#instructions)
+### created with HTML, CSS, Javascript (Vanilla)
 
-## Instructions
+### Project Description
+The **Ice Crime** website is a landing page, created as part of my FEND Nanodegree course.
+The task was to build a multi section landing page with dynamically built navigation, _scroll to top_ button and 'highlighted' sections - for user to see what section he is actively viewing. In addition the navigation items when clicked smoothly scroll to the corresponding section.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+### Building
+To build I followed the following steps: 
 
-To get started, open `js/app.js` and start building out the app's functionality
+1. Setting up the environment
+1. Building out my HTML with at least three content sections
+1. Building the navigation menu
+1. Adding functionality to distinguish the section in view
+1. Adding functionality to scroll to sections
+1. Refactoring and testing the performance
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+### Result 
+![About Page](https://github.com/tem-nik/Project-Previews/blob/master/Ice-Crime-About.png)
+![About Page](https://github.com/tem-nik/Project-Previews/blob/master/Ice-Crime-Sections.png)
+
--- js/app.js
+++ js/app.js
@@ -1,60 +1,59 @@
-// VARIABLES
+//Global Variables
 const navbar = document.querySelector('nav');
-const navbarList = document.getElementById('navbar__list');
+const navbarList = document.querySelector('.navbar__list');
 const sections = document.querySelectorAll('section');
 const fragment = document.createDocumentFragment();
+const anchorlinks = navbarList.querySelectorAll('a[href^="#"]');
 
 // FUNCTIONS
+//Function to create menu items, with each navbart item pointing to the corresponding section id
 
-// <----addNavbarItem---->
-// for each <section> element the function creates corresponding navigation item pointing to the section ID.
-function addNavbarItem() {
+function addNavbarList() {
   sections.forEach((section, i) => {
-    const navbarItem = document.createElement('li');
-    navbarItem.setAttribute('class', 'navbar__menu');
-    navbarItem.innerHTML = `<a href=#section${i} class="menu__link">${section.dataset.nav}</a>`;
-    fragment.appendChild(navbarItem);
+    const menuItem = document.createElement('li');
+    menuItem.setAttribute('class', 'menu__item');
+    menuItem.innerHTML = `<a href=#section${i} class="menu__link">${section.dataset.nav}</a>`;
+    fragment.appendChild(menuItem);
   });
 }
-addNavbarItem();
+addNavbarList();
 navbarList.appendChild(fragment);
 
-// <----addActive---->
-// for each section in viewport the function adds and removes 'active' class and highlights section to the user, it also adds a 'current' state to corresponding navigation item
-function addActive() {
-  sections.forEach(section => {
-    const bounding = section.getBoundingClientRect();
-    const sectionLink = document.querySelector(`a[href="#${section.getAttribute('id')}"]`);
-    const sectionHalfShown = section.offsetTop - (section.offsetHeight / 2);
-    const sectionBehind = section.offsetTop + (section.offsetHeight / 2);
-    if (
-      (bounding.top >= 0) &&
-      (bounding.left >= 0) &&
-      (Math.floor(bounding.right) <= window.innerWidth) &&
-      (window.pageYOffset > sectionHalfShown) && (window.pageYOffset <= sectionBehind)) {
-      section.classList.add('active');
-      sectionLink.classList.add('current');
-    } else if (window.pageYOffset >= sectionBehind || window.pageYOffset < section.offsetTop) {
-      section.classList.remove('active');
-      sectionLink.classList.remove('current');
-    }
-  })
-}
 
-// <----scrollToSection---->
-// for each navigation item clicked, this function scrolls to the corresponding section (section which id is the same as item's href)
-function scrollToSection(e) {
-  navbarList.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    e.preventDefault();
+// function to scroll to each section while menu is clicked in the navbar
+function scrollTo(e) {
+  anchorlinks.forEach(anchor => {
     if (anchor === e.target) {
+      e.preventDefault();
       document.querySelector(e.target.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth',
+        behavior: 'smooth'
       });
     }
   })
 }
 
+// <----addActive---->
+// highlights section either when scrolled to or clicked
+function Active() {
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    const Links = document.querySelector(`a[href="#${section.getAttribute('id')}"]`);
+    const sectionHalfShown = section.offsetTop - (section.offsetHeight / 2);
+    const sectionBehind = section.offsetTop + (section.offsetHeight / 2);
+    if (
+      (rect.top >= 0) &&
+      (rect.left >= 0) &&
+      (Math.floor(rect.right) <= window.innerWidth) &&
+      (window.pageYOffset > sectionHalfShown) && (window.pageYOffset <= sectionBehind)) {
+      section.classList.add('active');
+      Links.classList.add('current');
+    } else if (window.pageYOffset >= sectionBehind || window.pageYOffset < section.offsetTop) {
+      section.classList.remove('active');
+      Links.classList.remove('current');
+    }
+  })
+}
 
 // EVENT LISTENERS
-window.addEventListener('scroll', addActive);
-navbarList.addEventListener('click', scrollToSection);
+window.addEventListener('scroll', Active);
+navbarList.addEventListener('click', scrollTo);
